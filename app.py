d409a986a08022b7633f1f332c22501a1456cdd0
Faical Allou <faical.allou@gmail.com>
2021-11-14 13:57:46 +0100

    hello there api

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, send_file, request, redirect, url_for, abort
 from flask_cors import CORS
+from werkzeug.serving import WSGIRequestHandler
+
+import os, os.path
 
 app = Flask(__name__)
 CORS(app)
@@ -7,3 +10,15 @@ CORS(app)
 @app.route('/')
 def hello():
     return 'Hello there'
+
+
+if __name__ == '__main__':
+
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+    if os.environ.get('ON_HEROKU'):
+        port = int(os.environ.get('PORT'))
+        print(port)
+        app.run(host='0.0.0.0', port=port)
+    else :
+        print('local')
+        app.run(host='0.0.0.0', port=8080, debug=True)
